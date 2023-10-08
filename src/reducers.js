7aa5953158c934cd02d211846266717b834e8da6
Pratiknilopant <mr.pratiknilopant@gmail.com>
2023-10-08 10:42:41 +0530

    final commit

--- src/reducers.js
+++ src/reducers.js
@@ -1,5 +1,36 @@
 import { v4 as uuidv4 } from "uuid";
 
-const reducer = (state, action) => {};
+const initialState = {
+    users: [],
+    walletBalance: 0,
+  };
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'ADD_USER':
+          const newUser = {
+            id: uuid(),
+            name: action.payload.name,
+            balance: 10,
+          };
+          return {
+            ...state,
+            users: [...state.users, newUser],
+            walletBalance: state.walletBalance + 10,
+          };
+    
+        case 'HANDLE_FUEL':
+          if (state.walletBalance >= 50) {
+            return {
+              ...state,
+              walletBalance: state.walletBalance - 50,
+            };
+          }
+          return state;
+    
+        default:
+          return state;
+      }    
+};
 
 export default reducer;
